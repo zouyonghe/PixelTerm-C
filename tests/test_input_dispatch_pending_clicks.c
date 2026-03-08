efd7d0262fe9ddc7251c8b23bc5e1acb5c525288
邹永赫 <1259085392@qq.com>
2026-03-08 13:12:56 +0900

    test(input): make preview round-trip test meaningful

--- tests/test_input_dispatch_pending_clicks.c
+++ tests/test_input_dispatch_pending_clicks.c
@@ -25,7 +25,7 @@ static void test_preview_click_clears_when_mode_changes_without_hooks(void) {
     g_assert_cmpint(g_input_dispatch_stub_state.preview_click_calls, ==, 0);
 }
 
-static void test_preview_click_does_not_replay_after_mode_round_trip(void) {
+static void test_preview_click_processes_new_pending_after_mode_round_trip(void) {
     PixelTermApp app = {0};
 
     input_dispatch_test_reset_stubs();
@@ -39,11 +39,12 @@ static void test_preview_click_does_not_replay_after_mode_round_trip(void) {
     input_dispatch_process_pending_clicks(&app);
 
     app_set_mode(&app, APP_MODE_PREVIEW);
+    app.input.preview_click.pending = TRUE;
     app.input.preview_click.pending_time = g_get_monotonic_time() - 500000;
     input_dispatch_process_pending_clicks(&app);
 
     g_assert_false(app.input.preview_click.pending);
-    g_assert_cmpint(g_input_dispatch_stub_state.preview_click_calls, ==, 0);
+    g_assert_cmpint(g_input_dispatch_stub_state.preview_click_calls, ==, 1);
     g_assert_cmpint(g_input_dispatch_stub_state.preview_render_calls, ==, 0);
     g_assert_cmpint(g_input_dispatch_stub_state.preview_selection_render_calls, ==, 0);
 }
@@ -51,6 +52,6 @@ static void test_preview_click_does_not_replay_after_mode_round_trip(void) {
 void register_input_dispatch_pending_clicks_tests(void) {
     g_test_add_func("/input_dispatch_pending_clicks/preview_click/clears_when_mode_changes_without_hooks",
                     test_preview_click_clears_when_mode_changes_without_hooks);
-    g_test_add_func("/input_dispatch_pending_clicks/preview_click/does_not_replay_after_mode_round_trip",
-                    test_preview_click_does_not_replay_after_mode_round_trip);
+    g_test_add_func("/input_dispatch_pending_clicks/preview_click/processes_new_pending_after_mode_round_trip",
+                    test_preview_click_processes_new_pending_after_mode_round_trip);
 }
